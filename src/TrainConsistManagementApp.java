66754505b832a1990dd49e43b3c8bfad6c5184b3
ArjunRA2411026010211 <as9081@srmist.edu.in>
2026-05-02 15:33:36 +0530

    UC8: Filter passenger bogies using Streams

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -25,7 +25,47 @@ public class TrainConsistManagementApp {
         List<Bogie> bogies = new ArrayList<>();
 
         bogies.add(new Bogie("Sleeper", 72));
-        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("import java.util.*;\n" +
+                "import java.util.stream.*;\n" +
+                "\n" +
+                "class Bogie {\n" +
+                "    String name;\n" +
+                "    int capacity;\n" +
+                "\n" +
+                "    Bogie(String name, int capacity) {\n" +
+                "        this.name = name;\n" +
+                "        this.capacity = capacity;\n" +
+                "    }\n" +
+                "\n" +
+                "    public String toString() {\n" +
+                "        return name + \"(\" + capacity + \")\";\n" +
+                "    }\n" +
+                "}\n" +
+                "\n" +
+                "public class TrainConsistManagementApp {\n" +
+                "\n" +
+                "    public static void main(String[] args) {\n" +
+                "\n" +
+                "        System.out.println(\"====================================\");\n" +
+                "        System.out.println(\"UC8 - Filter Passenger Bogies\");\n" +
+                "        System.out.println(\"====================================\\n\");\n" +
+                "\n" +
+                "        List<Bogie> bogies = new ArrayList<>();\n" +
+                "\n" +
+                "        bogies.add(new Bogie(\"Sleeper\", 72));\n" +
+                "        bogies.add(new Bogie(\"AC Chair\", 60));\n" +
+                "        bogies.add(new Bogie(\"First Class\", 50));\n" +
+                "        bogies.add(new Bogie(\"Second Sitting\", 80));\n" +
+                "\n" +
+                "        // FILTER: capacity > 60\n" +
+                "        List<Bogie> filtered = bogies.stream()\n" +
+                "                .filter(b -> b.capacity > 60)\n" +
+                "                .collect(Collectors.toList());\n" +
+                "\n" +
+                "        System.out.println(\"Filtered Bogies:\");\n" +
+                "        System.out.println(filtered);\n" +
+                "    }\n" +
+                "}AC Chair", 60));
         bogies.add(new Bogie("First Class", 50));
 
         // Sort using Comparator (ascending order)
